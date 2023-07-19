4c7abfa141d95bc259454ffaad94a95a2f9b8fe3
salmanandoe <123531091+salmanandoe@users.noreply.github.com>
2023-07-19 10:31:15 -0400

    routes created

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home")
+    path('', views.Home.as_view(), name="home"),
+    path('profile/', views.Profile.as_view(), name="profile"),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View 
+from django.http import HttpResponse 
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+
+
+class Profile(TemplateView):
+    template_name = "profile.html"
+    
+    
