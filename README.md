74b270361df9684ea3467a274bfe912b0f909a07
salmanandoe <123531091+salmanandoe@users.noreply.github.com>
2023-07-20 11:34:08 -0400

    ready to deploy

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PocketRx
-Manage your medications all in one place!
+
+In this app you will be able to store all your medications and medical history in one place so that you have it at your fingertips when needed! You will also be able to access a medication database where you can find information about your current prescriptions as well. Enjoy!
--- pocketrx_project/settings.py
+++ pocketrx_project/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
 
+import environ
+environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,13 +67,17 @@ WSGI_APPLICATION = 'pocketrx_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pocketrx',
-    }
-}
 
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'pocketrx',
+    'USER': os.environ['DB_USER'],
+    'PASSWORD': os.environ['DB_PW'],
+    'HOST': os.environ['DB_HOST'],
+    'PORT': '5432',
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
